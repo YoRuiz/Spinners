c9b08f3c9eed32f4cdebcc134dd9bc83f1fff0a8
jajimenez <jarjcfgs@gmail.com>
2016-11-02 12:04:09 +0100

    commit con codigo del libro

--- app/src/main/java/com/example/jajimenez/spinners/MainActivity.java
+++ app/src/main/java/com/example/jajimenez/spinners/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.jajimenez.spinners;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity
+        implements AdapterView.OnItemSelectedListener {
+    private TextView selection;
+    private static final String[] items={"lorem", "ipsum", "dolor",
+            "sit", "amet",
+            "consectetuer", "adipiscing", "elit", "morbi", "vel",
+            "ligula", "vitae", "arcu", "aliquet", "mollis",
+            "etiam", "vel", "erat", "placerat", "ante",
+            "porttitor", "sodales", "pellentesque", "augue", "purus"};
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+
+        super.onCreate(icicle);
         setContentView(R.layout.activity_main);
+        selection=(TextView)findViewById(R.id.selection);
+        Spinner spin=(Spinner)findViewById(R.id.spinner);
+        spin.setOnItemSelectedListener(this);
+        ArrayAdapter<String> aa=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,items);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(aa);
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent,
+                               View v, int position, long id) {
+        selection.setText(items[position]);
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        selection.setText("");
     }
 }
